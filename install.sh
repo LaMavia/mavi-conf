34be2c78cbb4212e3a0e9d3875e2f4d241936d2b
Zuzanna Surowiec <ts438730@students.mimuw.edu.pl>
2023-04-24 19:47:59 +0200

    add first step param

--- install.sh
+++ install.sh
@@ -6,13 +6,25 @@ source ./scripts/helpers.sh
 RUN_FINAL="cd $(pwd)"
 export RUN_FINAL
 
-run ./steps/yay.sh
-run ./steps/brew.sh
-run ./steps/glab.sh
-run ./steps/gh.sh
-run ./steps/haskell.sh
-run ./steps/sys-packages.sh
-run ./steps/memory.sh
-run ./steps/gnome.sh
-run ./steps/zsh.sh
-run ./steps/code-config.sh
+steps=(yay brew glab gh haskell sys-packages memory gnome zsh code-config)
+step0=$1
+
+# Determine the index of the first step
+declare i=0
+for j in $(seq 0 $((${#steps[@]} - 1))); do
+  s=${steps[$j]}
+
+  if [[ "$s" == "$step0" ]]; then
+    i=$j
+    break
+  fi
+done
+
+# Run each step starting at step0
+j=0
+for step in "${steps[@]}"; do
+  if [[ $j -ge $i ]]; then
+    run "./steps/$step.sh"
+  fi
+  ((j = j + 1))
+done
